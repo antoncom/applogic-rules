69090027fe734c852d9fbc2dbe8f8786ad0170cf
Nikita <nikita.pymitron@gmail.com>
2025-05-26 11:27:53 +0700

    added vars and error msg

--- src/modifier/lua_func.lua
+++ src/modifier/lua_func.lua
@@ -13,10 +13,26 @@ function lua_func(varname, mdf_name, rule)
 
     local from_input = (not varlink.source) and mdf_name:sub(1,1) == "1"
 
+    local vars = {}
+
+    -- logic from util/substitute.lua
+    for name, _ in pairs(rule.setting) do
+        if name ~= varname then
+            vars[name] = rule.setting[name].output
+        else
+            if from_input then
+                vars[name] = rule.setting[name].input
+            else
+                vars[name] = rule.setting[name].subtotal
+            end
+        end
+    end
+
     if type(func) == "function" then
-        noerror, tmp_res = pcall(func)
+        noerror, tmp_res = pcall(func, vars)
 
         if noerror == false then
+            print('Error: ' .. tostring(tmp_res))
             tmp_res = nil
         end
     else
