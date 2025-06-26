6c53586b51745c0de53422e27bf966832ccc4ac2
Nikita <nikita.pymitron@gmail.com>
2025-06-26 13:34:18 +0700

    skip operator added

---
+++ src/operator/skip.lua
@@ -0,0 +1,46 @@
+local func_vars_builder = require "applogic.util.func_vars_builder"
+local func_debug = require "applogic.util.func_debug"
+
+-- operator: skip
+-- ["skip"] = function(vars) return <logical expression> end
+local function skip(rule, node_name, op_name, op_body, op_index)
+    local var_debug
+    if rule.debug_mode.enabled then var_debug = require "applogic.var.debug" end
+
+    local node_table = rule.setting[node_name]
+    local from_input = (not node_table.source) and (op_index == 1)
+    local vars = func_vars_builder.make_vars(node_name, rule, from_input)
+
+    local result = false
+    local noerror, tmp_res
+
+    if type(op_body) == "function" then
+        noerror, tmp_res = pcall(op_body, vars)
+    else
+        result = true -- skip if not a function
+        tmp_res = nil
+    end
+
+    if tmp_res == nil then
+        noerror = false
+        result = true -- skip anyway if error in lua chank
+    else
+        result = tmp_res
+    end
+
+    if rule.debug_mode.enabled then
+        local output_info
+
+        if type(op_body) == "function" then
+            output_info = func_debug.generate_output_info(func)
+        else
+            output_info = "not a function"
+        end
+
+        var_debug(node_name, rule):modifier(op_name, output_info, result, noerror)
+    end
+
+    return result
+end
+
+return skip
