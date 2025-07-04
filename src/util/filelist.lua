2cb4153521f606b345995c68823e9a89e00b30a4
Nikita <nikita.pymitron@gmail.com>
2025-07-04 13:45:54 +0700

    filelist file returned

---
+++ src/util/filelist.lua
@@ -0,0 +1,10 @@
+local util = require 'luci.util'
+
+local function filelist(pg)
+    local ls = util.exec("ls " .. pg.path .. " | grep '" .. pg.grep .. "'")
+    ls = util.split(ls, "\n")
+    ls[#ls] = nil -- remove blank element
+    return ls
+end
+
+return filelist
