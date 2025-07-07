969784158eeb96a421ef6b17c18474bff954d8eb
Nikita <nikita.pymitron@gmail.com>
2025-07-07 13:31:43 +0700

    default variable check added, input variable removed

--- src/operator/main.lua
+++ src/operator/main.lua
@@ -17,19 +17,15 @@ function main:run_node(node_name, rule)
     if rule.debug_mode.enabled then debug = require "applogic.var.debug" end
     local node_table = rule.setting[node_name]
 
-    -- Before the modifier applies, we put load the initial (input) value to intermediate (subtotal)
-    --node_table.subtotal = node_table.subtotal or string.format("%s", tostring(node_table.input))
-    if(node_table["saved"]) then
-        node_table.input = node_table["saved"]
+    if node_table["saved"] then
+        node_table.output = tostring(node_table["saved"])
+    elseif node_table["frozee"] then
+        node_table.output = tostring(node_table["frozee"])
+    elseif node_table["default"] then
+        node_table.output = tostring(node_table["default"])
+    else
+        node_table.output = ""
     end
-
-    -- TODO refactor the frozen modifier!
-    -- TMPL
-    if(node_table["frozee"]) then
-        node_table.input = node_table["frozee"]
-    end
-
-    node_table.output = node_table.output or tostring(node_table.input)
 
     for operator_index, operator_table in ipairs(node_table) do
         local operator_name, operator_body
@@ -42,16 +38,15 @@ function main:run_node(node_name, rule)
 
         if not node_table.frozen  then
             if "skip" == operator_name then
-                local is_skip = skip(rule, node_name, operator_name, operator_body, operator_index)
+                local is_skip = skip(rule, node_name, operator_name, operator_body)
 
                 if is_skip then
-                    -- Если указать у переменной input = "some value"
+                    -- Если указать у переменной default = "some value"
                     -- то перед отменой обработки присвоить переменной значение из input
                     -- иначе в переменной останется хранится последнее расчётное значение (из output)
-                    --node_table.subtotal = node_table.input or node_table.output or ""
-                    if rule["default"][node_name].input then
-                        node_table.input = string.format("%s", rule["default"][node_name].input) -- cloning default input
-                        node_table.output = string.format("%s",node_table.input)
+                    if rule["default"][node_name].default then
+                        node_table.default = string.format("%s", rule["default"][node_name].default)
+                        node_table.output = string.format("%s",node_table.default)
                     else
                         node_table.output = string.format("%s",node_table.output)
                     end
@@ -60,40 +55,40 @@ function main:run_node(node_name, rule)
             end
 
             if "func" == operator_name then
-                node_table.output = func(rule, node_name, operator_name, operator_body, operator_index)
+                node_table.output = func(rule, node_name, operator_name, operator_body)
             end
 
             if "bash" == operator_name then
-                node_table.output = bash(rule, node_name, operator_name, operator_body, operator_index)
+                node_table.output = bash(rule, node_name, operator_name, operator_body)
             end
 
             if "save" == operator_name then
-                node_table.output = save(rule, node_name, operator_name, operator_body, operator_index)
+                node_table.output = save(rule, node_name, operator_name, operator_body)
             end
 
             if "load-ubus" == operator_name then
-                node_table.output = load_ubus(rule, node_name, operator_name, operator_body, operator_index)
+                node_table.output = load_ubus(rule, node_name, operator_name, operator_body)
             end
 
             if "load-rule" == operator_name then
-                node_table.output = load_rule(rule, node_name, operator_name, operator_body, operator_index)
+                node_table.output = load_rule(rule, node_name, operator_name, operator_body)
             end
 
             if "subscribe" == operator_name then
-                load_subscribed(rule, node_name, operator_name, operator_body, operator_index)
+                load_subscribed(rule, node_name, operator_name, operator_body)
             end
 
             if "ui-update" == operator_name then
-                ui_update(rule, node_name, operator_name, operator_body, operator_index)
+                ui_update(rule, node_name, operator_name, operator_body)
             end
 
             if "store-db" == operator_name then
-                store_db(rule, node_name, operator_name, operator_body, operator_index)
+                store_db(rule, node_name, operator_name, operator_body)
             end
         end
 
         if "frozen" == operator_name then
-            frozen(rule, node_name, operator_name, operator_body, operator_index)
+            frozen(rule, node_name, operator_name, operator_body)
         end
     end
 
