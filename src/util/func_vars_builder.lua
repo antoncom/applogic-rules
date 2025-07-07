1dc81cfc8f2acd40aefa6a625026a37f39e4ae6d
Nikita <nikita.pymitron@gmail.com>
2025-07-07 13:33:27 +0700

    output-only as a source

--- src/util/func_vars_builder.lua
+++ src/util/func_vars_builder.lua
@@ -1,20 +1,12 @@
 local func_vars_builder = {}
 
--- logic from util/substitute.lua
-function func_vars_builder.make_vars(varname, rule, from_input)
+function func_vars_builder.make_vars(rule)
     local vars = {}
 
     for name, _ in pairs(rule.setting) do
-        if name ~= varname then
-            vars[name] = rule.setting[name].output
-        else
-            if from_input then
-                vars[name] = rule.setting[name].input
-            else
-                vars[name] = rule.setting[name].output
-            end
-        end
+        vars[name] = rule.setting[name].output
     end
+
     return vars
 end
 
