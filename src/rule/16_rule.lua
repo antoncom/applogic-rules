915ebcac186e95ff67d8a1632b79bee284459d41
Nikita <nikita.pymitron@gmail.com>
2026-03-24 15:05:18 +0700

    sms_answer, journal nodes updated

--- src/rule/16_rule.lua
+++ src/rule/16_rule.lua
@@ -1,5 +1,6 @@
 local debug_mode = require "applogic.debug_mode"
 local rule_init = require "applogic.operator.rule_init"
+local uci = require "luci.model.uci".cursor()
 
 
 local rule = {}
@@ -26,6 +27,11 @@ local rule_setting = {
 		},
 		{
 			["func"] = function (nodes)
+
+                -- print(type(nodes.received_sms))
+                -- print(nodes.received_sms)
+                -- print(luci.jsonc.parse(nodes.received_sms))
+
                 local lua_table = luci.jsonc.parse(nodes.received_sms) or {}
 				return lua_table or ""
 			end,
@@ -69,11 +75,6 @@ local rule_setting = {
         },
         {
             ["func"] = function (nodes)
-                -- print('tsmsmscomm_run_result type: ', type(nodes.tsmsmscomm_run_result))
-                -- print('tsmsmscomm_run_result: ', nodes.tsmsmscomm_run_result)
-
-                -- local lua_table = luci.jsonc.parse(nodes.tsmsmscomm_run_result)
-
                 if type(nodes.tsmsmscomm_run_result) == "table" then
                     local file = io.open(nodes.tsmsmscomm_run_result.tmp_file, "r")
                     if file ~= nil then
@@ -126,68 +127,73 @@ local rule_setting = {
         -- },
     },
 
+    email_answer = {
+        title = [[ Отправляет результат по email, если текст более чем max_text_size ]],
 
+        {
+            ["skip"] = function (nodes)
+                if nodes.tsmsmscomm_run_result == nil or
+                    type(nodes.tsmsmscomm_run_result) ~= "table" or
+                    nodes.tsmsmscomm_run_result.run == nil or
+                    nodes.tsmsmscomm_run_result.run == false
+                then
+                    return true
+                end
 
+                return (nodes.tsmsmscomm_run_result.result == nil or #nodes.tsmsmscomm_run_result.result == 0)
+                    or #nodes.tsmsmscomm_run_result.result <= tonumber(nodes.max_text_size)
+            end
+        },
+        {
+            ["load-ubus"] = function (nodes)
+                local params = {
+                    from = uci:get("tsmail", "general", "auth_user"),
+                    to = nodes.tsmsmscomm_run_result.trusted_email,
+                    subj = "Результат выполнения смс команды",
+                    body = "Результат выполнения смс команды",
+                    attach = nodes.tsmsmscomm_run_result.tmp_file,
+                }
 
+                return ({
+                    object = "tsmail",
+                    method = "send",
+                    params = params,
+                })
+            end
+        },
+    },
 
-    -- send_result_via_email = {
-    --     title = [[ Отправляет результат по email, если текст более чем max_text_size ]],
+    journal = {
+        {
+            ["skip"] = function (nodes)
+                if nodes.tsmsmscomm_run_result == nil or
+                    type(nodes.tsmsmscomm_run_result) ~= "table" or
+                    nodes.tsmsmscomm_run_result.run == nil or
+                    nodes.tsmsmscomm_run_result.run == false
+                then
+                    return true
+                end
 
-    --     {
-    --         ["skip"] = function (nodes)
-    --             if nodes.run_cmd == nil or #nodes.run_cmd == 0 then return true end
-
-    --             local run_cmd = luci.jsonc.parse(nodes.run_cmd)
-    --             if run_cmd.status == false then
-    --                 return true
-    --             end
-
-    --             return (run_cmd.result == nil or #run_cmd.result == 0) or #run_cmd.result <= tonumber(nodes.max_text_size)
-    --         end
-    --     },
-    --     {
-    --         ["func"] = function (nodes)
-    --             local run_cmd = luci.jsonc.parse(nodes.run_cmd)
-    --             return run_cmd.result
-    --         end
-    --     },
-    --     {
-    --         ["send-email"] = {
-    --             to = "$trusted_email",
-    --             subj = "Результат выполнения команды",
-    --             body = "Результат выполнения команды",
-    --             attach = "/tmp/sms_command_output.txt",
-    --         }
-    --     }
-    -- },
-
-    -- journal = {
-	-- 	{
-	-- 		["skip"] = function (nodes)
-    --             if nodes.run_cmd == nil or #nodes.run_cmd == 0 then return true end
-    --             local run_cmd = luci.jsonc.parse(nodes.run_cmd)
-    --             return run_cmd.status == false
-	-- 		end
-	-- 	},
-	-- 	{
-	-- 		["func"] = function (nodes)
-	-- 			local received_sms = luci.jsonc.parse(nodes.received_sms)
-    --             local run_cmd = luci.jsonc.parse(nodes.run_cmd)
-	-- 			return({
-	-- 				datetime = received_sms.date,
-	-- 				name = "Получена SMS-команда",
-	-- 				source = received_sms.sender,
-	-- 				command = received_sms.message,
-	-- 				response = run_cmd.result,
-	-- 			})
-	-- 		end
-	-- 	},
-	-- 	{
-	-- 		["store-db"] = {
-	-- 			param_list = { "journal" }
-	-- 		},
-	-- 	}
-	-- },
+                return (nodes.tsmsmscomm_run_result.result == nil or #nodes.tsmsmscomm_run_result.result == 0)
+            end
+        },
+		{
+			["func"] = function (nodes)
+				return({
+					datetime = os.date("%Y-%m-%d %H:%M:%S"),
+					name = "Получена SMS-команда",
+					source = nodes.tsmsmscomm_run_result.trusted_phone,
+					command = nodes.tsmsmscomm_run_result.shell_command,
+					response = nodes.tsmsmscomm_run_result.result,
+				})
+			end
+		},
+		{
+			["store-db"] = {
+				param_list = { "journal" }
+			},
+		}
+	},
 }
 
 function rule:make()
@@ -203,24 +209,8 @@ function rule:make()
     self:follow("call_tsmsmscomm_run"):debug()
     self:follow("tsmsmscomm_run_result"):debug()
     self:follow("sms_answer"):debug()
-
-
-
-    -- self:follow("new_sms_phone"):debug()
-    -- self:follow("new_sms_message"):debug()
-
-    -- self:follow("trusted_phone"):debug()
-    -- self:follow("allowed_command"):debug()
-    -- self:follow("trusted_email"):debug()
-
-    -- self:follow("run_cmd"):debug()
-
-
-
-    -- self:follow("send_result_via_sms"):debug()
-    -- self:follow("send_result_via_email"):debug()
-
-    -- self:follow("journal"):debug()
+    self:follow("email_answer"):debug()
+    self:follow("journal"):debug()
 end
 
 local metatable = {
