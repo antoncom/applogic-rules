f85353a88d22b311ce2a757b0fa2412c1dbdc25c
Nikita <pns@rosze.ru>
2026-06-11 16:15:53 +0700

    03_rule: tsmstm -> tsmslot, sim_id for balance, timer via websocket fix

---
+++ src/rule/03_rule.lua
@@ -0,0 +1,318 @@
+local debug_mode = require "applogic.debug_mode"
+local rule_init = require "applogic.util.rule_init"
+
+
+
+local rule = {}
+local rule_setting = {
+	title = {
+		input = "Правило переключения Сим-карты, если баланс ниже минимума",
+	},
+
+	slotinfo = {
+		note = [[ Данные о слотах Сим ]],
+		{
+			["load-ubus"] = function (nodes)
+				return {
+					object = "tsmslot",
+					method = "info",
+					params = {},
+				}
+			end
+		},
+		{	-- Если идёт процесс переключения, то пропускаем дальнейшую обработку правила
+			["break"] = function (nodes)
+				local last_switch_time = nodes.slotinfo.last_switch_time or 0
+				if ((os.time() - last_switch_time) < 30) then return true end
+			end
+		},
+	},
+
+	sim_found = {
+		note = [[ Сим-карта в слоте? "true" / "false" ]],
+		{
+			["load-ubus"] = function (nodes)
+				return {
+					object = "tsmodem.driver",
+					method = "cpin",
+					params = {},
+				}
+			end
+		},
+		{	-- Если нет симки в слоте, то пропускаем дальнейшую обработку правила
+			["break"] = function (nodes)
+				return (nodes.sim_found.value ~= "true")
+			end
+		}
+	},
+
+	provider_detected = {
+		note = [[ Идентификатор провайдера после автоопределения ]],
+		{
+			["load-ubus"] = function (nodes)
+				return {
+					object = "tsmodem.driver",
+					method = "provider_name",
+					params = {},
+				}
+			end
+		},
+		{
+			["break"] = function (nodes)
+				-- Перываем обработку правила если не получено имя провайдера и его код
+				local provider_name = nodes.provider_detected.value
+				local provider_id = nodes.provider_detected.comment
+				return not (tonumber(provider_id) and provider_id ~= "0" and nodes.provider_detected.value ~= "")
+			end
+		},
+
+	},
+
+	uci_provider_config = {
+        note = [[ Настройки провайдера для GSM-оператора активной симки ]],
+		{
+			["load-ubus"] = function (nodes)
+				local provider_id = nodes.provider_detected.comment
+				return {
+					object = "uci",
+					method = "get",
+					params = {
+						config = "tsmodem_adapter_provider",
+						section = provider_id,
+						option = nil
+					},
+				}
+			end
+		},
+    },
+
+	uci_slot_config = {
+		note = [[ Настройки активного слота SIM-карты ]],
+		{
+			["load-ubus"] = function (nodes)
+				return {
+					object = "uci",
+					method = "get",
+					params = {
+						config = "tsmodem",
+						section = "sim_" .. tostring(nodes.slotinfo.slot),
+						option = nil
+					},
+				}
+			end
+		},
+	},
+
+
+	check_balance_on_sim_registered = {
+	    note = [[ Отправляем SMS-запрос о балансе кактолько симка зарегистрировалась ]],
+		{
+			["skip"] = function (nodes)
+				-- Если CREG установился в 1 и с этого момента прошло более 10 сек
+				return ((tonumber(nodes.sim_found.updated) - tonumber(nodes.sim_found.changed)) > 10)
+			end
+		},
+		{
+			["load-ubus"] = function (nodes)
+				return {
+					object = "tsmodem.sms",
+		        	method = "send_sms",
+		        	params = {
+		        		phone = nodes.uci_provider_config.values.balance_sms_phone, 
+		        		text = nodes.uci_provider_config.values.balance_sms_text
+		        	},
+		        }
+	    	end
+		},
+		{
+			-- Запросив баланс сразу после регистации Симки, замораживаем, чтобы не было повторов ubus-запроса в эти 10 сек.
+			["frozen"] = function (nodes)
+				return 10
+			end
+		},
+	},
+
+	check_balance_daily = {
+	    note = [[ Отправляем SMS-запрос о балансе раз в день ]],
+	    {
+			["timeout"] = function(nodes)
+				return 86400	-- число секунд в 24-х часах
+			end
+		},
+
+		{
+			["skip"] = function (nodes)
+				return (nodes.check_balance_daily.value > 0)
+			end
+		},
+		{
+			["load-ubus"] = function (nodes)
+				return {
+					object = "tsmodem.sms",
+		        	method = "send_sms",
+		        	params = {
+		        		phone = nodes.uci_provider_config.values.balance_sms_phone, 
+		        		text = nodes.uci_provider_config.values.balance_sms_text
+		        	},
+		        }
+	    	end
+		},
+		{
+			["frozen"] = function (nodes)
+				return 5
+			end
+		},
+	},
+
+	actual_balance = {
+	    note = [[ Текущий статус баланса (число, * (значит в процессе), либо "" - если последний запрос был неудачен) ]],
+		{
+			["load-ubus"] = function (nodes)
+				return {
+					object = "tsmodem.driver",
+		        	method = "balance",
+		        	params = {},
+		        }
+	    	end
+		},
+		{
+			["websocket"] = function (nodes)
+				return {
+					sim_id = tostring(nodes.slotinfo.slot),
+					sim_balance = nodes.actual_balance
+				}
+			end
+		},
+		{
+			["break"] = function (nodes)
+				local BALANCE_MIN = tonumber(nodes.uci_slot_config.values.balance_min) or 0
+				local BALANCE_ACTUAL = tonumber(nodes.actual_balance.value) or 0
+				return (BALANCE_ACTUAL >= BALANCE_MIN)
+			end
+		}
+	},
+	timeout = {
+		note = [[ Таймер ожидания при низком балансе  ]],
+		{   -- Запускаем таймер
+			["timeout"] = function(nodes)
+				return tonumber(nodes.uci_slot_config.values.timeout_bal)
+			end
+		},
+		{
+			["websocket"] = function(nodes)
+				return({
+					sim_id = tostring(nodes.slotinfo.slot),
+					timeout = nodes.timeout and nodes.timeout.inited or "60",
+					wait_timer = nodes.timeout and tostring(nodes.timeout.value) or "60",
+					sim_balance = nodes.actual_balance
+				})
+			end
+		},
+	},
+	switch = {
+		note = [[ Переключить слот Сим-карт  ]],
+		{
+			["break"] = function (nodes)
+				if nodes.timeout.value > 0 then
+					return true
+				else
+					return false
+				end
+			end
+		},
+		{
+			["func"] = function (nodes)
+				local new_slotid = nil
+				local current_slotid = nodes.slotinfo.slot
+				if(current_slotid == 0) then new_slotid = "1" else new_slotid = "0" end
+				return new_slotid
+			end
+		},
+		{
+			["load-ubus"] = function (nodes)
+				return {
+					object = "tsmslot",
+					method = "switch",
+					params = { slotid = nodes.switch },
+					cached = "no",
+				}
+			end
+		},
+		{
+			["journal"] = function (nodes)
+				return({
+					datetime = os.date("%Y-%m-%d %H:%M:%S"),
+					name = 'Переключение Sim-слота (баланс ниже минимума)',
+					source = "STM32 (03_rule)",
+					command = "ubus call tsmslot switch",
+					response = "started"
+				})
+			end
+		},
+		{
+			["frozen"] = function (nodes)
+				return 30
+			end
+		},
+	},
+
+}
+
+-- Use "ERROR", "INFO" to override the debug level
+-- Use /etc/config/applogic to change the debug level
+-- Use :debug(ONLY) - to debug single variable in the rule
+-- Alternatively, you may run debug via shell like this "applogic 01_rule title sim_id" (use 5 variable names maximum)
+function rule:make()
+	debug_mode.level = "ERROR"
+	rule.debug_mode = debug_mode
+	local ONLY = rule.debug_mode.level
+
+	-- These variables are included into debug overview (run "applogic debug" to get all rules overview)
+	-- Green, Yellow and Red are measure of importance for Application logic
+	-- Green is for timers and some passive variables,
+	-- Yellow is for that vars which switches logic - affects to normal application behavior
+	-- Red is for some extraordinal application behavior, like watchdog, etc.
+	local overview = {
+		["reset_modem"] = { ["yellow"] = [[ return ($reset_modem == "true") ]] },
+		["do_switch"] = { ["yellow"] = [[ return ($do_switch == "true") ]] },
+		["sim_found"] = { ["yellow"] = [[ return ($sim_found == "false" or $sim_found == "*") ]] },
+		["reset_timer"] = { ["yellow"] = [[ return (tonumber($reset_timer) and tonumber($reset_timer) > 0) ]] },
+		["wait_timer"] = { ["yellow"] = [[ return (tonumber($wait_timer) and tonumber($wait_timer) > 0) ]] },
+	}
+
+	-- Пропускаем выполнние правила, если tsmodem automation == "stop"
+	if rule.parent.state.mode == "stop" then return end
+
+
+	self:follow("title"):debug()
+	self:follow("slotinfo"):debug()			-- Пропускаем все узлы ниже, если прошло не более 20 сек после начала переключения слотов
+	self:follow("sim_found"):debug()		-- Пропускаем все узлы ниже, если симка найдена в слоте
+	self:follow("provider_detected"):debug()-- Определяем какой провайдер определился на Сим-карте
+	self:follow("uci_provider_config"):debug()-- Получаем кортокий тел.номер оператора для получения баланса через СМС
+	self:follow("uci_slot_config"):debug()	-- Получаем минимальный уровень баланса на Сим-карте
+	self:follow("check_balance_on_sim_registered"):debug()-- Посылаем SMS-команду получения баланса как только симка зарегистрировалась
+	self:follow("check_balance_daily"):debug()-- Посылаем SMS-команду получения баланса 1 раз в день
+	self:follow("actual_balance"):debug()		-- Текущее значение баланса
+	self:follow("timeout"):debug()
+	self:follow("switch"):debug()
+											
+
+	
+											-- Пропускаем все узлы ниже, если баланс на сике выше минимума
+											
+											-- Если таймаут вышел - переключаем слот
+
+
+
+	
+end
+
+local metatable = {
+    __call = function(table, parent)
+        local rule_init_table = rule_init(table, rule_setting, parent)
+        rule_init_table:make()
+        return rule_init_table
+    end
+}
+setmetatable(rule, metatable)
+return rule
